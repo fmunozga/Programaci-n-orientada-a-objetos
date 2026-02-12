77fb8818e92c0d7cd9aa401473444eba05fde782
fmunozga <fmunozga@unal.edu.co>
2026-02-12 14:50:51 -0500

    Refactor age calculation functions and print output

---
+++ Ejercicio resuelto No 4.py
@@ -0,0 +1,19 @@
+def edalber(a):
+    e = a * (2/3)
+    return e
+def edana(b): 
+    aa= b * (4/3)
+    return aa
+def edmama(c, d, e): 
+    m = c + d + e
+    return m
+
+
+juan = 9
+
+
+alber = int(edalber(juan))
+ana = int(edana(juan))
+mama = int(edmama(alber, ana, juan))
+
+print(f"La edad de es Mamá: {mama}\nLa edad de Alber: {alber}\nLa edad de Ana: {ana}\nLa edad de Juan: {juan}")
