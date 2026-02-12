9a0aef40c8d2689417c0f0cca1c49057127313a8
fmunozga <fmunozga@unal.edu.co>
2026-02-12 14:21:08 -0500

    Implement sum and equation functions in script
    Added functions to calculate sum and equation based on conditions.

--- ejercicio resuelto No 5.py
+++ ejercicio resuelto No 5.py
@@ -1,1 +1,22 @@
-# New content goes here
+def su(a,b,d):
+    if b == 20:
+        summ = a + b 
+    else:
+        summ = a + (b/d)
+    return summ
+
+
+def equis(c,d):
+    eq = c +(d**2)
+    return eq
+
+
+suma= 0
+x = 20
+y = 40
+
+suma = su(suma,x,y)
+x = equis(x,y)
+suma = su(suma,x,y)
+
+print(f"el valor de la suma es:{suma}")# New content goes here
